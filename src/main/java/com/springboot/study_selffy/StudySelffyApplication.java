240bb07c9dde1e166f7ae2babc77f081f87135e4
selffy <self-uong15@naver.com>
2022-07-12 20:53:09 +0900

    init commit

---
+++ src/main/java/com/springboot/study_selffy/StudySelffyApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.study_selffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySelffyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySelffyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/study_selffy/StudySelffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.study_selffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySelffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
